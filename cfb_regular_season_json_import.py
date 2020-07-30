ac7ab85fb0129d991c3a1c3f778fa7e7681bd8a6
r-craig73 <ron.craig@comcast.net>
2020-07-30 01:57:13 -0700

    minor refactoring, add main sections & two methods

--- cfb_regular_season_json_import.py
+++ cfb_regular_season_json_import.py
@@ -1,30 +1,40 @@
 import requests, json, time
 
-# college football year
-CFB_YEAR = 2018
+# College football constant variables
+CFB_YEAR, GAMES, VEGAS_LINES = 2018, 'games', 'lines'
 
-## requests and saves regular season stats
-reg = requests.get(f'https://api.collegefootballdata.com/games?year={CFB_YEAR}&seasonType=regular')
-basic_game_stats = reg.json()
-with open(f'cfb_json_{CFB_YEAR}\\basicGameStatsCFB-JSON-{CFB_YEAR}.json', 'w') as f:
-    json.dump(basic_game_stats, f)
+## Requests and saves regular season stats [games] or Vegas lines [lines]
+def cfb_stats(date, metric):
+    results = requests.get(f'https://api.collegefootballdata.com/{metric}?year={date}&seasonType=regular')
+    basic_stats = results.json()
+    if metric.lower() == 'games':
+        with open(f'cfb_json_{date}\\basicGameStatsCFB-JSON-{date}.json', 'w') as f:
+            json.dump(basic_stats, f)
+    elif metric.lower() == 'lines':
+        with open(f'cfb_json_{date}\\vegasLines-{date}.json', 'w') as f:
+            json.dump(basic_stats, f)
 
-## list games ID, greater 800 regular season games
-reg_cfb_game_id = []
-for i in range(0, len(basic_game_stats), 1):
-    reg_cfb_game_id.append(basic_game_stats[i]['id'])
-print(f'{CFB_YEAR} Regular season games: {len(basic_game_stats)}')
+# List games ID, greater than 800 regular season games
+def detailed_cfb_stats(date, metric):
+    regular_cfb_game_id = []
+    regular = requests.get(f'https://api.collegefootballdata.com/{metric}?year={date}&seasonType=regular')
+    basic_stats = regular.json()
+    for i in range(0, len(basic_stats), 1):
+        regular_cfb_game_id.append(basic_stats[i]['id'])
+    print(f'{date} Regular season games: {len(basic_stats)}')
 
-## requests and saves regular season's vegas lines
-line = requests.get(f'https://api.collegefootballdata.com/lines?year={CFB_YEAR}&seasonType=regular')
-vegas_lines = line.json()
-with open(f'cfb_json_{CFB_YEAR}\\vegasLines.json', 'w') as f:
-    json.dump(vegas_lines, f)
+    # Requests and saves detailed stats for the regular season (takes a loooong time, 15-45 minutes)
+    for i in range(0, len(basic_stats), 1):
+        r = requests.get(f'https://api.collegefootballdata.com/games/teams?year={date}&seasonType=regular&gameId={regular_cfb_game_id[i]}')
+        detailed_game_stats = r.json()
+        with open(f'cfb_json_{date}\\detailedGameStats-{regular_cfb_game_id[i]}.json', 'w') as f:
+            json.dump(detailed_game_stats, f)
+    time.sleep(2.5)
 
-## requests and saves detailed stats for the regular season (takes a loooong time, 15-45 minutes)
-for i in range(0, len(basic_game_stats), 1):
-    r = requests.get(f'https://api.collegefootballdata.com/games/teams?year={CFB_YEAR}&seasonType=regular&gameId={reg_cfb_game_id[i]}')
-    detailed_game_stats = r.json()
-    with open(f'cfb_json_{CFB_YEAR}\\detailedGameStats-{reg_cfb_game_id[i]}.json', 'w') as f:
-        json.dump(detailed_game_stats, f)
-    time.sleep(3)
+def main():
+    cfb_stats(CFB_YEAR, GAMES)
+    cfb_stats(CFB_YEAR, VEGAS_LINES)
+    detailed_cfb_stats(CFB_YEAR, GAMES)
+
+if __name__ == "__main__":
+    main()
