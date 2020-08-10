07fd696548fc7c235dbf5d4f3cd2a2491e87ad8a
r-craig73 <ron.craig@comcast.net>
2020-08-09 22:45:51 -0700

    join game detail stats to a 3xN DataFrame, 1 game

--- cfb_stats_jsons_to_dataframe.py
+++ cfb_stats_jsons_to_dataframe.py
@@ -24,22 +24,22 @@ df_id_teams = pd.concat([df_id, df_teams_basic], axis=1)
 team_0_stats = pd.DataFrame(df_teams.stats[0]).sort_values(by=['category'])
 team_1_stats = pd.DataFrame(df_teams.stats[1]).sort_values(by=['category'])
 
-# split teams 'DataFrame' into detailed teams stats, transposed
-team_0_stats = team_0_stats
-team_1_stats = team_1_stats
-
 # join teams stats into a 3xN matrix ('category', 'stat_x', 'stat_y')
 teams_detailed_stats = pd.merge(team_0_stats, team_1_stats, how='inner', on=['category']).sort_values(by=['category'])
 
 # transpose teams_detailed_stats 
 teams_transposed = teams_detailed_stats.transpose()
+teams_transposed = pd.DataFrame(teams_transposed.rename(columns=teams_transposed.iloc[0]).drop(teams_transposed.index[0]))
+teams_transposed = teams_transposed.set_index([pd.Index([0, 1])])
 
-print(df_id_teams)
-print(teams_detailed_stats)
+# join df_id_teams and teams_detailed_stats DataFrames
+teams_final = pd.concat([df_id_teams, teams_transposed], axis=1)
+
 print(teams_transposed)
+print(teams_final)
 
 # to do: [x] (1) keep the id value in the first column
 # to do: [x] (2) expand teams columns into other columns
 # to do: [x] join (1) and (2) to a 3x5 DataFrame
-# to do: [x] (3) expand each team detailed stats into other columns, 3xN DataFram
-# to do: [ ] join (1), (2), (3) to a 3xN DataFrame
+# to do: [x] (3) expand each team detailed stats into other columns, 3xN DataFrame
+# to do: [x] join (1), (2), (3) to a 3xN DataFrame
